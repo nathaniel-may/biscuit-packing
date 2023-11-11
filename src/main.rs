bea79e9c0172155d77fffa91cba1969ec37c0e05
Nathaniel May <nathanieldmay@gmail.com>
2023-11-10 20:28:09 -0500

    lil fixes

--- src/main.rs
+++ src/main.rs
@@ -9,38 +9,47 @@ use clap::Parser;
 fn main() {
     let args = cli::Args::parse();
 
-    let width = args.pan_width;
-    let length = args.pan_length;
+    // scale up so the svg comes out with reasonable dimensions
+    let width = args.pan_width * 10.0;
+    let length = args.pan_length * 10.0;
     let runs = args.runs;
 
     match args.command {
         cli::Commands::Single { biscuits } => {
             println!(
-                "optimizing placement of {biscuits} biscuits on a {width} X {length} pan with {runs} runs"
+                "optimizing placement of {biscuits} biscuits on a {} X {} pan with {runs} runs",
+                args.pan_width, args.pan_length
             );
 
             let points = run(biscuits, width, length, runs);
-            // for p in points {
-            //     println!("{}, {}", p.x, p.y)
-            // }
-            let rendered = render::render_packing(args.pan_width, args.pan_length, points);
-            let filename = format!("{biscuits}_biscuits_{width}X{length}_pan.svg");
+            for p in &points {
+                println!("{}, {}", p.x, p.y)
+            }
+            let rendered = render::render_packing(width, length, points);
+            let filename = format!(
+                "{biscuits}_biscuits_{}X{}_pan.svg",
+                args.pan_width, args.pan_length
+            );
             svg::save(filename, &rendered).unwrap();
         }
         // TODO check that start < end
         cli::Commands::Multi { start, end } => {
             println!(
-                "optimizing placement of biscuits from {start} to {end} on a {width} X {length} pan with {runs} runs"
+                "optimizing placement of biscuits from {start} to {end} on a {} X {} pan with {runs} runs",
+                args.pan_width, args.pan_length
             );
 
             for n in start..(end + 1) {
-                // println!(":: {n} biscuits ::");
+                println!(":: {n} biscuits ::");
                 let points = run(n, width, length, runs);
-                // for p in points {
-                //     println!("{}, {}", p.x, p.y)
-                // }
-                let rendered = render::render_packing(args.pan_width, args.pan_length, points);
-                let filename = format!("{n}_biscuits_{width}X{length}_pan.svg");
+                for p in &points {
+                    println!("{}, {}", p.x, p.y)
+                }
+                let rendered = render::render_packing(width, length, points);
+                let filename = format!(
+                    "{n}_biscuits_{}X{}_pan.svg",
+                    args.pan_width, args.pan_length
+                );
                 svg::save(filename, &rendered).unwrap();
             }
         }
--- src/render.rs
+++ src/render.rs
@@ -1,32 +1,32 @@
 use crate::point::Point;
-use svg::node::element::path::Data;
-use svg::node::element::{Circle, Path, SVG};
+use svg::node::element::{Circle, Rectangle, SVG};
 use svg::Document;
 
 pub fn render_packing(pan_width: f64, pan_length: f64, placement: Vec<Point>) -> SVG {
-    let pad = 10.0;
-    let data = Data::new()
-        .move_to((pad, pad))
-        .line_by((0, pan_length))
-        .line_by((pan_width, 0))
-        .line_by((0, -1.0 * pan_length))
-        .close();
+    let pad = 0.1 * pan_width;
+    let biscuit_radius = 0.02 * pan_width;
 
-    let pan = Path::new()
-        .set("fill", "none")
+    let pan = Rectangle::new()
+        // .set("x", pad)
+        // .set("y", pad)
+        .set("width", pan_width)
+        .set("height", pan_length)
+        .set("fill", "lightgrey")
         .set("stroke", "black")
-        .set("stroke-width", 1)
-        .set("d", data);
+        .set("stroke-width", 1);
 
     let biscuits = placement.iter().map(|p| {
         Circle::new()
-            .set("cx", p.x + pad)
-            .set("cy", p.y + pad)
-            .set("r", 1.0)
+            .set("cx", p.x)
+            .set("cy", p.y)
+            .set("r", biscuit_radius)
+            .set("fill", "darkgreen")
     });
 
     let image = Document::new()
-        .set("viewBox", (0, 0, pan_width + pad, pan_length + pad))
+        // .set("height", pan_length)
+        // .set("width", pan_width)
+        .set("viewBox", (0, 0, pan_length, pan_width))
         .add(pan);
 
     biscuits.fold(image, |image, b| image.add(b))
